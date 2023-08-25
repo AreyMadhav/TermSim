83ad872754fb6449a34d34e11f95a6d03cf17706
Madhav Tyagi <realmadhavtyagi@gmail.com>
2023-08-26 03:53:25 +0530

    Update shell.cpp

--- shell.cpp
+++ shell.cpp
@@ -17,6 +17,7 @@ struct CommandDB {
         commands.insert("pwd");
         commands.insert("sudo");
         commands.insert("ifconfig");
+        commands.insert("neofetch");
         // Add more commands
     }
 
@@ -64,21 +65,32 @@ struct TerminalShell {
 
         if (command == "pwd") {
             std::cout << currentDirectory << std::endl;
-        } else if (command == "ls") {
+        }
+        else if (command == "ls") {
             simulateLs();
-        } else if (command == "cd") {
+        }
+        else if (command == "cd") {
             simulateCd(tokens);
-        } else if (command == "mkdir") {
+        }
+        else if (command == "mkdir") {
             simulateMkdir(tokens);
-        } else if (command == "rm") {
+        }
+        else if (command == "rm") {
             simulateRm(tokens);
-        } else if (command == "cp") {
+        }
+        else if (command == "cp") {
             simulateCp(tokens);
-        } else if (command == "sudo") {
+        }
+        else if (command == "sudo") {
             simulateSudo(tokens);
-        } else if (command == "ifconfig") {
+        }
+        else if (command == "ifconfig") {
             simulateIfconfig();
-        } else if (!database.commandExists(command)) {
+        }
+        else if (command == "neofetch") {
+            simulateNeofetch();
+        }
+        else if (!database.commandExists(command)) {
             std::cout << "bash:" << command << ": command not found..." << std::endl;
         }
     }
@@ -92,7 +104,8 @@ struct TerminalShell {
     void simulateCd(const std::vector<std::string>& tokens) {
         if (tokens.size() < 2) {
             std::cout << "Usage: cd <directory>" << std::endl;
-        } else {
+        }
+        else {
             std::cout << "Changed directory to: " << tokens[1] << std::endl;
             currentDirectory = tokens[1];
         }
@@ -102,7 +115,8 @@ struct TerminalShell {
     void simulateMkdir(const std::vector<std::string>& tokens) {
         if (tokens.size() < 2) {
             std::cout << "Usage: mkdir <directory>" << std::endl;
-        } else {
+        }
+        else {
             std::cout << "Created directory: " << tokens[1] << std::endl;
         }
     }
@@ -111,7 +125,8 @@ struct TerminalShell {
     void simulateRm(const std::vector<std::string>& tokens) {
         if (tokens.size() < 2) {
             std::cout << "Usage: rm <file/directory>" << std::endl;
-        } else {
+        }
+        else {
             std::cout << "Removed: " << tokens[1] << std::endl;
         }
     }
@@ -120,7 +135,8 @@ struct TerminalShell {
     void simulateCp(const std::vector<std::string>& tokens) {
         if (tokens.size() < 3) {
             std::cout << "Usage: cp <source> <destination>" << std::endl;
-        } else {
+        }
+        else {
             std::cout << "Copied " << tokens[1] << " to " << tokens[2] << std::endl;
         }
     }
@@ -129,7 +145,8 @@ struct TerminalShell {
     void simulateSudo(const std::vector<std::string>& tokens) {
         if (tokens.size() < 4 || tokens[1] != "apt" || tokens[2] != "install") {
             std::cout << "Usage: sudo apt install {package}" << std::endl;
-        } else {
+        }
+        else {
             std::string package = tokens[3];
             std::cout << "Simulating installation of package: " << package << std::endl;
             std::cout << "Package " << package << " has been installed." << std::endl;
@@ -145,10 +162,28 @@ struct TerminalShell {
         std::cout << "        TX packets 133  bytes 21119 (20.6 KiB)" << std::endl;
         std::cout << "        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0" << std::endl;
     }
+    void simulateNeofetch() {
+    std::cout << "         .-/+oossssoo+/-.               " << "OS: Ubuntu" << std::endl;
+    std::cout << "     `:+ssssssssssssssssss+:`           " << "Host: user" << std::endl;
+    std::cout << "   -+ssssssssssssssssssyyssss+-         " << "Kernel: Linux" << std::endl;
+    std::cout << " .ossssssssssssssssssdMMMNysssso.       " << "Uptime: 1 hour" << std::endl;
+    std::cout << "/ssssssssssshdmmNNmmyNMMMMhssssss/      " << "Packages: 1000" << std::endl;
+    std::cout << "+sssshhhyNMMMyhhyyyyhmNMMMNhssss+      " << "Shell: Shell" << std::endl;
+    std::cout << "ossyNMMMNyMMh     /mMMMNyNMMNoss      " << "Resolution: 1920x1080" << std::endl;
+    std::cout << "+sssshhhyNMMMyhhyyyyhdNMMMNhssss+      " << "DE: Gnome" << std::endl;
+    std::cout << "/sssssssssssdmmNNNNNNmmNMMMMhssss/      " << "WM: Window Manager" << std::endl;
+    std::cout << ".ossssssssssssssssssdMMMNysssso.       " << "Theme: Gnome Black Theme" << std::endl;
+    std::cout << " -+sssssssssssssssssyyyssss+-         " << "Icons: Gnome Black Icons" << std::endl;
+    std::cout << "   -+ssssssssssssssssssyyssss+-         " << "Terminal: Terminal" << std::endl;
+    std::cout << "     `:+ssssssssssssssssss+:`           " << "CPU: Ryzen 9 3960x" << std::endl;
+    std::cout << "         .-/+oossssoo+/-               " << "GPU: RTX 4090" << std::endl;
+    std::cout << "                                        " << "Memory: 64 GB " << std::endl;
+    std::cout << "                                        " << "Disk: 256 GB" << std::endl;
+}
 };
 
 int main() {
     TerminalShell shell;
     shell.run();
     return 0;
-}
+};
